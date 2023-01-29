aa25077bcd3ad410272bb19eecd2f5892fc8c508
Zainab Fahim <80398726+Zainab-Fahim@users.noreply.github.com>
2023-01-29 10:19:35 +0530

    Fixed AppBreadcrumb bug

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -1,34 +1,51 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { CContainer, CHeader, CHeaderBrand, CHeaderNav, CHeaderToggler } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilMenu } from '@coreui/icons'
+import { useLocation } from 'react-router-dom'
 
-import { AppBreadcrumb } from './index'
-import slogo from 'src/assets/brand/supAut-logo.svg'
+import routes from '../routes'
 
-const AppHeader = () => {
-  const dispatch = useDispatch()
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react'
+
+const AppBreadcrumb = () => {
+  const currentLocation = useLocation().pathname
+
+  const getRouteName = (pathname, routes) => {
+    const currentRoute = routes.find((route) => route.path === pathname)
+    return currentRoute ? currentRoute.name : false
+  }
+
+  const getBreadcrumbs = (location) => {
+    const breadcrumbs = []
+    location.split('/').reduce((prev, curr, index, array) => {
+      const currentPathname = `${prev}/${curr}`
+      const routeName = getRouteName(currentPathname, routes)
+      routeName &&
+        breadcrumbs.push({
+          pathname: currentPathname,
+          name: routeName,
+          active: index + 1 === array.length ? true : false,
+        })
+      return currentPathname
+    })
+    return breadcrumbs
+  }
+
+  const breadcrumbs = getBreadcrumbs(currentLocation)
 
   return (
-    <CHeader position="sticky" className="mb-4">
-      <CContainer fluid>
-        <CHeaderToggler
-          className="ps-1"
-          onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
-        >
-          <CIcon icon={cilMenu} size="lg" />
-        </CHeaderToggler>
-        <CHeaderBrand className="mx-auto d-md-none" to="/">
-          <img className="d-block w-150" src={slogo} alt="slide 2" align="center" />
-        </CHeaderBrand>
-        <CHeaderNav className="d-none d-md-flex me-auto">
-          <AppBreadcrumb />
-        </CHeaderNav>
-      </CContainer>
-    </CHeader>
+    <CBreadcrumb className="m-0 ms-2">
+      <CBreadcrumbItem href="/">Home</CBreadcrumbItem>
+      {breadcrumbs.map((breadcrumb, index) => {
+        return (
+          <CBreadcrumbItem
+            {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
+            key={index}
+          >
+            {breadcrumb.name}
+          </CBreadcrumbItem>
+        )
+      })}
+    </CBreadcrumb>
   )
 }
 
-export default AppHeader
+export default React.memo(AppBreadcrumb)
