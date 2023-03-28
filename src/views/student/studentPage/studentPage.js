aa9cd53aa4c87a39f98dc0bc8e66c930ae664c9b
Zainab-Fahim <shafna.zainab.fahim@gmail.com>
2023-03-28 15:50:00 +0530

    <Updates logical and visual score based on number of times help requested

--- src/views/student/studentPage/studentPage.js
+++ src/views/student/studentPage/studentPage.js
@@ -19,6 +19,44 @@ import { cilHamburgerMenu } from '@coreui/icons'
 import { Link } from 'react-router-dom'
 import { CButton } from '@coreui/react/dist'
 
+async function updateSkillLevel(student) {
+  const grade = student.grade
+
+  // Get all students with the same grade
+  const querySnapshot = await db.collection('students').where('grade', '==', grade).get()
+
+  // Calculate the mean and standard deviation for help requests for logical and visual
+  const helpRequestCounts = querySnapshot.docs.map((doc) => doc.data().helpRequestCount)
+  const meanLogical =
+    helpRequestCounts.reduce((total, count) => total + count.logical, 0) / querySnapshot.size
+  const meanVisual =
+    helpRequestCounts.reduce((total, count) => total + count.visual, 0) / querySnapshot.size
+  const stdDevLogical = Math.sqrt(
+    helpRequestCounts.reduce(
+      (total, count) => total + Math.pow(count.logical - meanLogical, 2),
+      0,
+    ) / querySnapshot.size,
+  )
+  const stdDevVisual = Math.sqrt(
+    helpRequestCounts.reduce((total, count) => total + Math.pow(count.visual - meanVisual, 2), 0) /
+      querySnapshot.size,
+  )
+
+  // Calculate the z-score for the student's help request count for logical and visual
+  const zScoreLogical = (student.helpRequestCount.logical - meanLogical) / stdDevLogical
+  const zScoreVisual = (student.helpRequestCount.visual - meanVisual) / stdDevVisual
+
+  // Determine the skill level based on the z-score
+  const skill = {}
+  skill.logical = zScoreLogical * 10 + 50 // Scale the z-score to a value between 0 and 100
+  skill.visual = zScoreVisual * 10 + 50 // Scale the z-score to a value between 0 and 100
+  skill.timeManagement = student.skill.timeManagement
+  skill.creativity = student.skill.creativity
+
+  // Update the student's skill object in the database
+  await db.collection('students').doc(student.id).update({ skill })
+}
+
 const StudentPage = () => {
   const [student, setStudent] = useState([
     {
@@ -28,6 +66,7 @@ const StudentPage = () => {
       contact: '0776792726',
       careGiverEmail: 'doe@gmail.com',
       careGiverContact: '0776792724',
+      helpRequestCount: { logicalHelp: '0', visualHelp: '0' },
       skill: { creativity: '3', logical: '4', time: '4', visual: '5' },
     },
   ])
@@ -37,6 +76,7 @@ const StudentPage = () => {
         const studentInfoTable = []
         studentSnapshot.forEach((studentDoc) => {
           var studentInfo = studentDoc.data()
+          console.log(studentInfo)
           studentInfoTable.push({
             id: studentDoc.id,
             grade: studentInfo.grade,
@@ -44,6 +84,10 @@ const StudentPage = () => {
             contact: studentInfo.contact,
             careGiverEmail: studentInfo.careGiverEmail,
             careGiverContact: studentInfo.careGiverContact,
+            helpRequestCount: {
+              logicalHelp: studentInfo.helpRequestCount.logicalHelp,
+              visualHelp: studentInfo.helpRequestCount.visualHelp,
+            },
             skill: {
               creativity: studentInfo.skill.creativity,
               logical: studentInfo.skill.logical,
@@ -108,7 +152,11 @@ const StudentPage = () => {
                       <Link
                         to={`/student/${student.id}/${student.grade}/${student.name}/${student.contact}/${student.careGiverEmail}/${student.careGiverContact}/${student.skill.creativity}/${student.skill.logical}/${student.skill.time}/${student.skill.visual}`}
                       >
-                        <CButton color="primary" variant="outline">
+                        <CButton
+                          color="primary"
+                          variant="outline"
+                          onClick={() => updateSkillLevel(student)}
+                        >
                           Go to Page
                         </CButton>
                       </Link>
